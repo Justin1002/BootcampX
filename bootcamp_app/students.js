1fe8f0ab0ad4f0b535b58b57ac631cbb43def196
Justin Ly <jly3@ualberta.ca>
2021-03-02 22:10:51 +0000

    add student and teachers node database lookups

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,3 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const args = process.argv.slice(2)
+
+pool.query (`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON (cohorts.id = students.cohort_id)
+WHERE cohorts.name LIKE '%${args[0]}%'  
+LIMIT ${args[1] || 5};
+`)
+.then (res => {
+  for (obj of res.rows) {
+    console.log(`${obj.name} has an id of ${obj.student_id} and was in the ${obj.cohort} cohort`)
+  }
+})
+
+.catch (err => console.error('query error', err.stack));
+
